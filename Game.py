2a2275ab603af20297ce56218229c25b6388c799
AdrianEQuinonesPerez <adrian.quinones7@upr.edu>
2025-09-06 19:07:20 -0400

    Task 5, Part 1 (Attempt 2)

--- Game.py
+++ Game.py
@@ -12,7 +12,7 @@ def ball_movement(ball_speed=None, hit_sound=None):
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 7
+    speed = 5
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
